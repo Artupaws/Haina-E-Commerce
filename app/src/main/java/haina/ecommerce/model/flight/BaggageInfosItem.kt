468b1f1b6a28b5aa68fbf8f8000403b79faa161e
putradev <putraswa@gmail.com>
2021-06-17 15:59:41 +0700

    update

---
+++ app/src/main/java/haina/ecommerce/model/flight/BaggageInfosItem.kt
@@ -0,0 +1,21 @@
+package haina.ecommerce.model.flight
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class BaggageInfosItem(
+
+	@field:SerializedName("fare")
+	val fare: Int? = null,
+
+	@field:SerializedName("code")
+	val code: String? = null,
+
+	@field:SerializedName("currency")
+	val currency: String? = null,
+
+	@field:SerializedName("desc")
+	val desc: String? = null
+) : Parcelable
