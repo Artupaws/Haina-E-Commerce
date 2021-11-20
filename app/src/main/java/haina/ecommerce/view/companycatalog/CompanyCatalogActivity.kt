8a02a35d3832c7a22ca0db0acec725e4f0f77a83
Griffin <griffin.seannery@yahoo.com>
2021-11-20 11:29:07 +0700

    Update CompanyCatalogActivity.kt

---
+++ app/src/main/java/haina/ecommerce/view/companycatalog/CompanyCatalogActivity.kt
@@ -0,0 +1,18 @@
+package haina.ecommerce.view.companycatalog
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import haina.ecommerce.R
+import haina.ecommerce.databinding.ActivityCompanyCatalogBinding
+
+class CompanyCatalogActivity : AppCompatActivity(){
+
+    private lateinit var binding: ActivityCompanyCatalogBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCompanyCatalogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
