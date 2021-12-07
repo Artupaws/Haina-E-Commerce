81cc88744c3b49f1fd747e0742bc9a9dde7dbc5e
Griffin <griffin.seannery@yahoo.com>
2021-12-07 16:55:45 +0700

    company media list item

--- app/src/main/java/haina/ecommerce/adapter/companycatalog/items/CompanyItemMediaAdapter.kt
+++ app/src/main/java/haina/ecommerce/adapter/companycatalog/items/CompanyItemMediaAdapter.kt
@@ -3,15 +3,13 @@ package haina.ecommerce.adapter.companycatalog.items
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import haina.ecommerce.R
-import haina.ecommerce.databinding.ListItemCompanyMediaBinding
+import haina.ecommerce.databinding.ListItemCompanyItemMediaBinding
 
 class CompanyItemMediaAdapter() : RecyclerView.Adapter<CompanyItemMediaAdapter.Holder>() {
 
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): Holder {
-        val binding = ListItemCompanyMediaBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        val binding = ListItemCompanyItemMediaBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
 
         return Holder(binding)
     }
@@ -22,7 +20,7 @@ class CompanyItemMediaAdapter() : RecyclerView.Adapter<CompanyItemMediaAdapter.H
 
     override fun getItemCount() = 3
 
-    inner class Holder(var binding: ListItemCompanyMediaBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class Holder(var binding: ListItemCompanyItemMediaBinding) : RecyclerView.ViewHolder(binding.root){
 
     }
 
